0052a2beff2d8b5bb22bc919054e0179ed014e6c
taj <dtajfiroz@gmail.com>
2024-11-12 08:08:10 +0530

    commiting

---
+++ FinalVariable.java
@@ -0,0 +1,10 @@
+public class FinalVariable {
+public static void main(String args[]){
+    int x=10;
+    System.out.println(x);
+    x=20;
+    System.out.println(x);
+    final int y=40;
+    System.out.println(y);
+}
+}
